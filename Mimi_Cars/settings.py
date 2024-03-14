db076bb31331646b73935fcd7a205a7dd6350dbd
winning godspower <winninggodspower@gmail.com>
2024-03-14 14:35:35 +0100

    add progress libraries

--- Mimi_Cars/settings.py
+++ Mimi_Cars/settings.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,10 +78,10 @@ STATICFILES_FINDERS = (
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default= os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
